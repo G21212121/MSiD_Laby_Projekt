5f1b89f90846970e5b5d86e8c6c44c78cdacc946
Gasin <tgasinski2014@gmail.com>
2026-04-24 14:04:37 +0200

    Dodanie wstępnego kodu dla klasy Optimization_Algorithm.py

--- src/Optimization_Algorithm.py
+++ src/Optimization_Algorithm.py
@@ -7,3 +7,19 @@
 # Operacje:
 
 # + solve() -> tuple[list[int], float] (metoda abstrakcyjna zwracająca najlepszą trasę i jej koszt)
+
+# ======================================================================================================================
+
+from abc import ABC, abstractmethod
+from TSP_Problem import TSPProblem
+
+# Abstrakcyjna klasa bazowa dla wszystkich algorytmów metaheurystycznych:
+
+class OptimizationAlgorithm(ABC):
+    def __init__(self, problem: TSPProblem):
+        self._problem: TSPProblem = problem
+
+    # Metoda abstrakcyjną, którą musi zaimplementować każdy algorytm
+    @abstractmethod
+    def solve(self) -> tuple[list[int], float]:     # Zwraca krotkę (najlepsza trasa, całkowity koszt)
+        pass
